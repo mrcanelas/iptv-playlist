8c37e0d490c1cdecf2a70ab545011e14f9c2fac4
Silas Alves <sialsan@hotmail.com>
2021-04-22 16:49:53 -0300

    Update tvmagazine.com.br.config.js

--- sites/tvmagazine.com.br.config.js
+++ sites/tvmagazine.com.br.config.js
@@ -2,14 +2,11 @@ const dayjs = require('dayjs');
 const utc = require('dayjs/plugin/utc');
 const timezone = require('dayjs/plugin/timezone');
 const customParseFormat = require('dayjs/plugin/customParseFormat');
-const cheerio = require('cheerio');
-const needle = require('needle');
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(customParseFormat);
 
-const endpoint = 'https://www.tvmagazine.com.br';
 const br_date = dayjs.utc().tz('America/Sao_Paulo');
 
 module.exports = {
@@ -20,52 +17,30 @@ module.exports = {
 
   url({ date, channel }) {
     const [slug, id] = channel.site_id.split('/');
-    return `https://www.tvmagazine.com.br/programacao/${slug}/${id}/${br_date.format(
-      'DD/MM/YYYY'
+    return `https://94c8cb9f702d-tv-magazine-api.baby-beamup.club/${slug}/${id}/${br_date.format(
+      'DD-MM-YYYY'
     )}`;
   },
   logo({ content }) {
-    $ = cheerio.load(content);
-    const img = $('.logo').find('img').attr('src');
-    return endpoint + img;
+    const img = content[0].logo
+    return img;
   },
   parser({ content, date }) {
-    const programs = [];
-    $ = cheerio.load(content);
-    $('ul.ultimas > li').each((i, el) => {
-      const url = endpoint + $(el).find('a').attr('href');
-      const title = $(el).find('a').text();
-      const time = $(el).find('meta').attr('content');
-      const [icon, description, category] = needle.get(
-        url,
-        function (error, response) {
-          if (!error && response.statusCode == 200)
-          $ = cheerio.load(response.body);
-          const icon = endpoint + $('.fotos').find('a').attr('href');
-          const description = $('.infos').text();
-          const category = $('.fichatecnica').find('li').text();
-          const data = { icon, description, category };
-          return data;
-        }
-      );
-
-      if (title && time) {
-        const start = dayjs.utc(time, 'YYYY-MM-DD HH:mm:00-0300');          
-
-        if (programs.length && !programs[programs.length - 1].stop) {
-          programs[programs.length - 1].stop = start
-        }
-      }
-
-      programs.push({
+    const programs = content.map((el) => {
+      const title = el.title
+      const description = el.description
+      const icon = el.icon
+      const category = el.category
+      const time = el.start
+      const start = dayjs.utc(time, 'YYYY-MM-DD HH:mm:00-0300')
+      return {
         title,
         start,
-        icon,
         description,
         category,
-      });
-    });
-
+        icon
+      }
+    })
     return programs;
   },
 };
