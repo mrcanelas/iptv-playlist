e36aee4391490f0d60e2ce2e2afe17e201ec5d63
mrcanelas <sialsan@hotmail.com>
2021-07-30 15:40:45 -0300

    remove sites if dont work

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const channelsFile = "./sites/vivoplay.com.br.channels.xml";
 const endPoint = "https://mrcanelas.github.io/iptvcat-scraper/brazil.json";
 let playlistFileText = "#EXTM3U";
 
-const urlIgnore = [
-  "tv.factoryiptv.com",
-  "mains.services",
-  "dnsl.me:80",
-  "185.63.254.200:8080",
-  "cdn.us21.live:80",
-  "pascaltv.site:8080",
-];
+const urlIgnore = 
+  "tv.factoryiptv.com" ||
+  "mains.services" ||
+  "dnsl.me:80" ||
+  "185.63.254.200:8080" ||
+  "cdn.us21.live:80" ||
+  "pascaltv.site:8080"
+
 
 function parseName(value) {
   return value.toLowerCase().replace(/ /g, "");
@@ -26,13 +26,14 @@ async function genPlaylist() {
   const channels = site.elements.find((el) => el.name === "channels");
 
   channels.elements.map((el) => {
-    const url = resp.data.filter(
+    const url = resp.data.find(
       (item) =>
-        //        item.Link.includes('factoryiptv') &&
+        !item.Link.includes(urlIgnore) &&
         item.Format == "FHD 2K" &&
         parseName(item.Channel).includes(parseName(el.attributes.xmltv_id)) &&
         item.Status == "Online"
     );
+    if (url !== undefined) console.log("Encontrado links para o canal: " + el.attributes.xmltv_id)
     let itemHeader = "#EXTINF:-1,";
 
     if (el.attributes.xmltv_id)
@@ -42,7 +43,7 @@ async function genPlaylist() {
       itemHeader += ` tvg-name="${el.elements[0].text}"`;
 
     itemHeader += `,${el.elements[0].text}`;
-    itemHeader += `\n${url[0] !== undefined ? url[0].Link : ""}`;
+    itemHeader += `\n${url !== undefined ? url.Link : ""}`;
     playlistFileText += `\n${itemHeader}`;
 
   });
