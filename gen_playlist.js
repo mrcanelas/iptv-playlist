25185c8ee099bea8fd28e4e559aae918b6c2a92c
mrcanelas <sialsan@hotmail.com>
2024-02-25 12:56:14 -0300

    Change to vivo

--- gen_playlist.js
+++ gen_playlist.js
@@ -1,71 +1,86 @@
-const axios = require("axios");
 const fs = require("fs-extra");
 const parser = require("iptv-playlist-parser");
 const convert = require("xml-js");
 const channelsFile = "./sites/vivoplay.com.br.channels.xml";
-const staticChannels = fs.readFileSync("./static_channels.m3u", {
+const staticChannels = fs.readFileSync("./926057-full.m3u", {
   encoding: "utf-8",
 });
-const endPoint = "https://mrcanelas.github.io/iptvcat-scraper/brazil.json";
+
 let playlistFileText = "#EXTM3U";
 
-const urlIgnore =
-  "tv.factoryiptv.com" ||
-  "mains.services" ||
-  "dnsl.me:80" ||
-  "185.63.254.200:8080" ||
-  "cdn.us21.live:80" ||
-  "pascaltv.site:8080";
+const xml = fs.readFileSync(channelsFile, { encoding: "utf-8" });
+const result = convert.xml2js(xml);
+const site = result.elements.find((el) => el.name === "site");
+const channels = site.elements.find((el) => el.name === "channels");
+const resultStatic = parser.parse(staticChannels);
 
-function parseName(value) {
-  return value.toLowerCase();
+channels.elements.map((el) => {
+  const index = resultStatic.items.filter(
+    (val) => parseName(val.tvg.name) == parseName(el.attributes.xmltv_id)
+  );
+  const filterBestQuality = filterChannelsByQuality(index)
+  if (filterBestQuality[0] !== undefined) {
+    let itemHeader = "#EXTINF:-1,";
+
+    if (el.attributes.xmltv_id) itemHeader += ` tvg-id="${el.attributes.xmltv_id}"`;
+    if (el.attributes.logo) itemHeader += ` tvg-logo="${el.attributes.logo}"`;
+    if (el.attributes.xmltv_id) itemHeader += ` tvg-name="${el.attributes.xmltv_id}"`;
+    if (filterBestQuality[0]?.group?.title) itemHeader += ` group-title="${clearGroupName(filterBestQuality[0]?.group?.title)}"`;
+
+    itemHeader += `,${el.attributes.xmltv_id}`;
+    itemHeader += `\n${filterBestQuality[0].url}`;
+    playlistFileText += `\n${itemHeader}`;
+    console.log("Adicionado o canal: " + filterBestQuality[0].tvg.name)
+  }
+});
+fs.outputFile("./gh-pages/playlist.m3u", playlistFileText, (err) => {
+  console.log("Sucess");
+});
+
+function parseName(name) {
+  return name.toLowerCase()
+  .replace(/\[h265\]|\[h265\]/gi, "")
+  .replace(/fhd | fhd/g, "")
+  .replace(/hd | hd/g, "")
+  .replace(/sd | sd/g, "")
+  .replace(/&/g, '&amp;')
+  .replace(/ cam/g, "")
+  .trim()
+  .trim()
 }
 
-axios.get(endPoint).then((resp) => {
-  const xml = fs.readFileSync(channelsFile, { encoding: "utf-8" });
-  const result = convert.xml2js(xml);
-  const site = result.elements.find((el) => el.name === "site");
-  const channels = site.elements.find((el) => el.name === "channels");
+function getQualityScore(name) {
+  const qualityWeights = [
+    { quality: "[H265]", weight: 1 },
+    { quality: "FHD", weight: 2 },
+    { quality: "HD", weight: 3 },
+    { quality: "SD", weight: 4 },
+  ];
 
-  channels.elements.map((el) => {
-    const result = parser.parse(staticChannels);
-    const index = result.items.find(
-      (val) => val.tvg.name == el.attributes.xmltv_id
-    );
-    if (index !== undefined) {
-      let itemHeader = "#EXTINF:-1,";
-
-      if (index.tvg.id) itemHeader += ` tvg-id="${index.tvg.id}"`;
-      if (index.tvg.logo) itemHeader += ` tvg-logo="${index.tvg.logo}"`;
-      if (index.tvg.name) itemHeader += ` tvg-name="${index.tvg.name}"`;
-
-      itemHeader += `,${index.name}`;
-      itemHeader += `\n${index.url}`;
-      playlistFileText += `\n${itemHeader}`;
-    } else {
-      const url = resp.data.find(
-        (item) =>
-          !item.Link.includes(urlIgnore) &&
-          item.Format == "FHD 2K" &&
-          parseName(item.Channel).includes(parseName(el.attributes.xmltv_id)) &&
-          item.Status == "Online"
-      );
-      if (url !== undefined)
-        console.log("Encontrado links para o canal: " + el.attributes.xmltv_id);
-      let itemHeader = "#EXTINF:-1,";
-
-      if (el.attributes.xmltv_id)
-        itemHeader += ` tvg-id="${el.attributes.xmltv_id}"`;
-      if (el.attributes.logo) itemHeader += ` tvg-logo="${el.attributes.logo}"`;
-      if (el.attributes.xmltv_id)
-        itemHeader += ` tvg-name="${el.elements[0].text}"`;
-
-      itemHeader += `,${el.elements[0].text}`;
-      itemHeader += `\n${url !== undefined ? url.Link : ""}`;
-      playlistFileText += `\n${itemHeader}`;
+  const lowercaseName = name.toLowerCase();
+  
+  for (const { quality, weight } of qualityWeights) {
+    if (lowercaseName.includes(quality.toLowerCase())) {
+      return weight;
     }
+  }
+
+  return qualityWeights.length + 1; // default score for other cases
+}
+
+function filterChannelsByQuality(channels) {
+  return channels.sort((a, b) => {
+    const scoreA = getQualityScore(a.tvg.name);
+    const scoreB = getQualityScore(b.tvg.name);
+
+    return scoreA - scoreB; // ascending order, change to b - a for descending order
   });
-  fs.outputFile("./gh-pages/playlist.m3u", playlistFileText, (err) => {
-    console.log("Sucess");
-  });
-});
+}
+
+function clearGroupName(name) {
+  return name
+  .replace("Canais", "")
+  .replace("|", "")
+  .replace("PT: ", "")
+  .trim()
+}
